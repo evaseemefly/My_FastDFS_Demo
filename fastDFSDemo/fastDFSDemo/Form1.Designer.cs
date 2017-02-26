f3f82271ed4a0769e0bb3ca2659b1e42c3d5193a
t420s <evaseemefly@126.com>
2017-02-26 20:27:08 +0800

    家中写的fastDFS demo

---
+++ fastDFSDemo/fastDFSDemo/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace fastDFSDemo
+{
+    partial class FastDF
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtTracker = new System.Windows.Forms.TextBox();
+            this.txtStorage = new System.Windows.Forms.TextBox();
+            this.btnLinkFastDfs = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "traker服务器列表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "storage服务器列表";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "选择文件并上传";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 21);
+            this.textBox1.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(33, 217);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(512, 184);
+            this.listBox1.TabIndex = 9;
+            // 
+            // txtTracker
+            // 
+            this.txtTracker.Location = new System.Drawing.Point(33, 51);
+            this.txtTracker.Multiline = true;
+            this.txtTracker.Name = "txtTracker";
+            this.txtTracker.Size = new System.Drawing.Size(200, 103);
+            this.txtTracker.TabIndex = 10;
+            // 
+            // txtStorage
+            // 
+            this.txtStorage.Location = new System.Drawing.Point(254, 51);
+            this.txtStorage.Multiline = true;
+            this.txtStorage.Name = "txtStorage";
+            this.txtStorage.Size = new System.Drawing.Size(200, 103);
+            this.txtStorage.TabIndex = 11;
+            // 
+            // btnLinkFastDfs
+            // 
+            this.btnLinkFastDfs.Location = new System.Drawing.Point(470, 51);
+            this.btnLinkFastDfs.Name = "btnLinkFastDfs";
+            this.btnLinkFastDfs.Size = new System.Drawing.Size(75, 74);
+            this.btnLinkFastDfs.TabIndex = 12;
+            this.btnLinkFastDfs.Text = "连接FASTDFS";
+            this.btnLinkFastDfs.UseVisualStyleBackColor = true;
+            this.btnLinkFastDfs.Click += new System.EventHandler(this.btnLinkFastDfs_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(460, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "尚未连接FASTDFS";
+            // 
+            // FastDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 414);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLinkFastDfs);
+            this.Controls.Add(this.txtStorage);
+            this.Controls.Add(this.txtTracker);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FastDF";
+            this.Text = "FastDFS分布式文件系统";
+            this.Load += new System.EventHandler(this.FastDF_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtTracker;
+        private System.Windows.Forms.TextBox txtStorage;
+        private System.Windows.Forms.Button btnLinkFastDfs;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ fastDFSDemo/fastDFSDemo/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using FastDFS.Client;
+using FastDFS.Client.Common;
+using FastDFS.Client.Config;
+using FastDFS.Client.Storage;
+using FastDFS.Client.Tracker;
+
+
+namespace fastDFSDemo
+{
+    public partial class FastDF : Form
+    {
+        #region 公共变量
+        private List<IPEndPoint> trackerIPs = new List<IPEndPoint>();
+        private IPEndPoint endPoint;
+        private StorageNode storageNode;
+        #endregion
+        public FastDF()
+        {
+            InitializeComponent();
+            txtTracker.Text = "xxx.xxx.xxx.xxx";
+            txtStorage.Text = "xxx.xxx.xxx.xxx";
+        }
+        /// <summary>
+        /// 连接FASTDFS服务器
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLinkFastDfs_Click(object sender, EventArgs e)
+        {
+            string[] trackers = txtTracker.Text.Replace("\r\n", ",").Split(new char[','], StringSplitOptions.RemoveEmptyEntries);
+            string[] storages = txtTracker.Text.Replace("\r\n", ",").Split(new char[','], StringSplitOptions.RemoveEmptyEntries);
+            foreach (var onetracker in trackers)
+            {
+                trackerIPs.Add(new IPEndPoint(IPAddress.Parse(onetracker), 22122));
+            }
+            ConnectionManager.Initialize(trackerIPs);
+            storageNode = FastDFSClient.GetStorageNode("group1");
+            label3.Text = "连接成功";
+        }
+        /// <summary>
+        /// 浏览文件并上传
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "文本文件|*.*|png|*.png|JPG|*.jpg|JPEG|*.jpeg|所有文件|*.*";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fName = openFileDialog.FileName;
+                byte[] content = null;
+                /*  FileInfo fileInfo = new FileInfo(filePath);
+                  return fileInfo.Length;*/
+                FileStream streamUpload = new FileStream(fName, FileMode.Open);
+                using (BinaryReader reader = new BinaryReader(streamUpload))
+                {
+                    content = reader.ReadBytes((int)streamUpload.Length);
+                }
+                textBox1.Text = fName;
+             
+                //主文件
+                string fileName = FastDFSClient.UploadFile(storageNode, content, "png");
+                var info = FastDFSClient.GetFileInfo(storageNode, fileName);
+                //从文件
+                var slaveFileName = FastDFSClient.UploadSlaveFile("group1", content, fileName, "_120x120", "png");
+                var slaveInfo = FastDFSClient.GetFileInfo(storageNode, slaveFileName);
+                listBox1.Items.Add(string.Format("主文件：http://{0}:8080/group1/{1}", trackerIPs[0].Address, fileName));
+                listBox1.Items.Add(string.Format("主文件大小：{0}KB,创建时间：{1}", info.FileSize, info.CreateTime));
+                listBox1.Items.Add(string.Format("从文件：http://{0}:8080/group1/{1}", trackerIPs[0].Address, slaveFileName));
+                listBox1.Items.Add(string.Format("从文件大小：{0}KB,创建时间：{1}", slaveInfo.FileSize, slaveInfo.CreateTime));
+
+
+            }
+
+        }
+        #region
+
+        private void DownloadFile1(string orgUrl, string targetPath)
+        {
+            using (WebClient web = new WebClient())
+            {
+                web.DownloadFile(orgUrl, targetPath);
+            }
+        }
+        private void DownloadFile2(string fileName, string targetPath)
+        {
+            //  byte[] buffer = FastDFSClient.DownloadFile(node, fileName, 0L, 0L);
+            targetPath = "d:\\" + Path.GetFileName(fileName);
+            FDFSFileInfo fileInfo = FastDFSClient.GetFileInfo(storageNode, fileName);
+            if (fileInfo.FileSize >= 1024)//如果文件大小大于1KB  分次写入
+            {
+                FileStream fs = new FileStream(targetPath, FileMode.OpenOrCreate, FileAccess.Write);
+                long offset = 0;
+                long len = 1024;
+                while (len > 0)
+                {
+                    byte[] buffer = new byte[len];
+                    buffer = FastDFSClient.DownloadFile(storageNode, fileName, offset, len);
+                    fs.Write(buffer, 0, int.Parse(len.ToString()));
+                    fs.Flush();
+                    // setrichtext(name_ + "已经下载：" + (offset / fileInfo.FileSize) + "%");
+                    offset = offset + len;
+                    len = (fileInfo.FileSize - offset) >= 1024 ? 1024 : (fileInfo.FileSize - offset);
+                }
+                fs.Close();
+
+            }
+            else//如果文件大小小小于1KB  直接写入文件
+            {
+                byte[] buffer = new byte[fileInfo.FileSize];
+                buffer = FastDFSClient.DownloadFile(storageNode, fileName);
+                FileStream fs = new FileStream(targetPath, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        #endregion
+
+        private void FastDF_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
